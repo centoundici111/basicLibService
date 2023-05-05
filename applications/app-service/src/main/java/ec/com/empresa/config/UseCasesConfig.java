6fefffaae3e7b611fb8e2b57ce4c73b1cbb6e750
Gaby <paogaby0803@gmail.com>
2023-05-04 22:07:46 -0500

    chore: initial setup

---
+++ applications/app-service/src/main/java/ec/com/empresa/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package ec.com.empresa.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "ec.com.empresa.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
